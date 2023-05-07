c26805f7e81e9e12e6e3b76ee95068d903939c84
Pugnator <ookami@mail.ru>
2023-05-07 13:45:02 +0200

    Addded file logging

--- log.cc
+++ log.cc
@@ -7,15 +7,39 @@ void VoidTracer::Critical(const std::string &message) {}
 void VoidTracer::Error(const std::string &message) {}
 void VoidTracer::Fatal(const std::string &message) {}
 
-void FileTracer::Info(const std::string &message) {}
-void FileTracer::Debug(const std::string &message) {}
-void FileTracer::Warning(const std::string &message) {}
-void FileTracer::Critical(const std::string &message) {}
-void FileTracer::Error(const std::string &message) {}
-void FileTracer::Fatal(const std::string &message) {}
+void FileTracer::Info(const std::string &message)
+{
+  std::time_t t = std::time(nullptr);
+  std::tm tm = *std::localtime(&t);
+  // Write the log message to the file
+  file_handle_ << std::put_time(&tm, "[%Y-%m-%d %H:%M:%S] ") << message;
+}
+void FileTracer::Debug(const std::string &message)
+{
+  std::string header("Debug: ");
+  Info(header + message);
+}
+void FileTracer::Warning(const std::string &message)
+{
+  std::string header("Warning: ");
+  Info(header + message);
+}
+void FileTracer::Critical(const std::string &message)
+{
+  std::string header("CRITICAL: ");
+  Info(header + message);
+}
+void FileTracer::Error(const std::string &message)
+{
+  std::string header("ERROR: ");
+  Info(header + message);
+}
+void FileTracer::Fatal(const std::string &message)
+{
+}
 
 #if ((defined(WIN32) || defined(__MINGW32__) || defined(__MINGW64__)))
-    
+
 void ConsoleTracer::Info(const std::string &message)
 {
   if (std_out_ == INVALID_HANDLE_VALUE)
@@ -26,32 +50,61 @@ void ConsoleTracer::Info(const std::string &message)
   WriteConsole(std_out_, message.c_str(), message.length(), &dwBytesWritten, NULL);
 };
 
-#endif //#if defined(WIN32) || #if defined(__MINGW32__) || #if defined(__MINGW64__)
+#endif // #if defined(WIN32) || #if defined(__MINGW32__) || #if defined(__MINGW64__)
 
 void ConsoleTracer::Debug(const std::string &message)
 {
   std::string header("Debug: ");
+  Info(header + message);
 }
 
 void ConsoleTracer::Warning(const std::string &message)
 {
   std::string header("Warning: ");
-}
-
-void ConsoleTracer::Critical(const std::string &message)
-{
-  std::string header("CRITICAL: ");
+  Info(header + message);
 }
 
 void ConsoleTracer::Error(const std::string &message)
 {
   std::string header("ERROR: ");
+  Info(header + message);
+}
+
+void ConsoleTracer::Critical(const std::string &message)
+{
+  std::string header("CRITICAL: ");
+  Info(header + message);
 }
 
 void ConsoleTracer::Fatal(const std::string &message)
 {
   std::string header("*** FATAL ***: ");
+  Info(header + message);
   //*((char *)0) = 0xDEADBEAF;
+}
+
+Log::Log()
+{
+  set_level(TraceSeverity::info);
+  HMODULE isFromDll = GetModuleHandle(NULL);
+  if (!isFromDll)
+  {
+    configure(TraceType::file);
+  }
+  else
+    configure(TraceType::console);
+}
+
+void Log::set_level(TraceSeverity level)
+{
+  // set the bit corresponding to the given severity and all lower severity levels
+  logging_level_ |= static_cast<uint32_t>(level) | static_cast<uint32_t>(TraceSeverity::info);
+}
+
+bool Log::is_severity_enabled(TraceSeverity level)
+{
+  // check if the bit corresponding to the given severity is set
+  return (logging_level_ & static_cast<uint32_t>(level)) != 0;
 }
 
 void Log::configure(TraceType lt)
@@ -65,9 +118,11 @@ void Log::configure(TraceType lt)
     instance_ = std::make_unique<ConsoleTracer>();
     break;
   case TraceType::file:
+    //provide some kind of a log file path here
     instance_ = std::make_unique<FileTracer>();
     break;
   default:
+    // not implemented yet
     break;
   }
 }
--- log.hpp
+++ log.hpp
@@ -1,18 +1,26 @@
 #pragma once
 
+/*! \file A simple logger implementation */
+
 #if !defined(__GNUC__) && !defined(__clang__)
 #error "Designed for GCC only"
 #endif
 
+#include <mutex>
 #include <iostream>
+#include <fstream>
+#include <filesystem>
 #include <memory>
 #include <fmt/format.h>
 #include <windows.h>
 
+//! Macroses for simple usage.
 #define LOG(...) LOG_INFO(__VA_ARGS__)
+#define LOG_LEVEL(x) Log::get().set_level(x)
 #define LOG_INFO(...) Log::get().log(TraceSeverity::info, __VA_ARGS__)
 #define LOG_DEBUG(...) Log::get().log(TraceSeverity::debug, __VA_ARGS__)
 
+//! A tracer type class enumerator.
 enum class TraceType
 {
   devnull,
@@ -25,41 +33,76 @@ enum class TraceType
 #endif
 };
 
+//! A tracer severity class enumerator.
 enum class TraceSeverity
 {
-  info,
-  debug,
-  warning,
-  error,
-#if defined(__ARM_EABI__)
-  critical,
-  fatal,
-#endif
+  info = 1,
+  warning = 2,
+  error = 4,
+  critical = 8,
+  fatal = 16,
+  debug = 32
 };
 
+//! A tracer abstract interface.
 class Tracer
 {
 public:
   virtual ~Tracer() {}
+  /**
+   * @brief Generic informational message
+   */
   virtual void Info(const std::string &message) = 0;
+  /**
+   * @brief A message usually only needed for debug purposes.
+   */
   virtual void Debug(const std::string &message) = 0;
+  /**
+   * @brief A warning an user should pay an attention to.
+   */
   virtual void Warning(const std::string &message) = 0;
-  virtual void Critical(const std::string &message) = 0;
+  /**
+   * @brief  A problem that can cause the system or application to malfunction or produce incorrect results,
+   * but it does not necessarily bring the system or application down completely.
+   */
   virtual void Error(const std::string &message) = 0;
+
+  /**
+   * @brief A serious problem that can cause significant impact to the system or application,
+   * but the system or application can still continue to function with some limitations or degradation of service.
+   */
+  virtual void Critical(const std::string &message) = 0;
+  /**
+   * @brief A problem that causes the system or application to crash or become completely non-functional.
+   */
   virtual void Fatal(const std::string &message) = 0;
 };
 
+//! A file tracer. Logs messages to a file.
 class FileTracer : public Tracer
 {
 public:
+  FileTracer(const std::filesystem::path &filepath = "log.txt")
+  {
+    if (!file_handle_.is_open())
+    {
+      // what to do here?
+    }
+    file_handle_ = std::ofstream(filepath, std::ios::app);
+  }
   void Info(const std::string &message) override;
   void Debug(const std::string &message) override;
   void Warning(const std::string &message) override;
-  void Critical(const std::string &message) override;
   void Error(const std::string &message) override;
+  void Critical(const std::string &message) override;
   void Fatal(const std::string &message) override;
+
+private:
+  //! A handle to a filestream.
+  std::ofstream file_handle_;
 };
 
+//! A void tracer. Used when you want to silent all message or there is nowhere to output.
 class VoidTracer : public Tracer
 {
 public:
@@ -71,6 +114,7 @@ public:
   void Fatal(const std::string &message) override;
 };
 
+//! A console/terminal tracer.
 class ConsoleTracer : public Tracer
 {
 public:
@@ -91,24 +135,46 @@ public:
   void Fatal(const std::string &message) override;
 
 private:
+  //! A handle to a terminal.
   HANDLE std_out_;
 };
 
+//! A log singletone facade.
 class Log
 {
 public:
   Log(Log const &) = delete;
   void operator=(Log const &) = delete;
 
-  static Log &get(TraceType type = TraceType::console)
+  /**
+   * @brief Get an instance of a logger.
+   *
+   * @param type A desired tracer type.
+   * @return Log& A logger's singletone instance
+   */
+  static Log &get()
   {
-    static Log instance(type);
+    static Log instance;
     return instance;
   }
 
+  /**
+   * @brief A main logging entry point for an user.
+   *
+   * @tparam S
+   * @tparam Args
+   * @param severity a log message severity level
+   * @param format
+   * @param args
+   */
   template <typename S, typename... Args>
   void log(TraceSeverity severity, const S &format, Args &&...args)
   {
+    if (!is_severity_enabled(severity))
+    {
+      // This channel is muted.
+      return;
+    }
     std::string message = fmt::vformat(format, fmt::make_format_args(args...));
     switch (severity)
     {
@@ -118,15 +184,36 @@ public:
     case TraceSeverity::debug:
       instance_->Debug(message);
       break;
+    default:
+      // not implemented yet
+      break;
     }
   }
+  /**
+   * @brief Set desired logger's level
+   *
+   * @param level A severity level.
+   */
+  void set_level(TraceSeverity level);
+
+  //! Configures enabled tracer.
+  void configure(TraceType lt);
 
 private:
-  Log(TraceType type = TraceType::console)
-  {
-    configure(TraceType::console);
-  }
+  //! Hidden singletone constructor.
+  Log();
 
-  void configure(TraceType lt);
+  /**
+   * @brief Checks against enable severity levels.
+   *
+   * @param level severity level to check.
+   * @return true if level is enabled.
+   * @return false if level is muted.
+   */
+  bool is_severity_enabled(TraceSeverity level);
+
+  //! Stores enabled severity level.
+  uint32_t logging_level_;
+  //! An instance of the actual worker tracer.
   std::unique_ptr<Tracer> instance_;
 };
